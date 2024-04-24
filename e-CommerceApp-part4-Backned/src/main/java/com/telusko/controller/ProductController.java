affe59e2c1b33f0c263238753014d62c7534cbe6
Gaurav Sharma <gsharma1154@gmail.com>
2024-04-24 19:32:45 +0530

    added update and delete

---
+++ e-CommerceApp-part4-Backned/src/main/java/com/telusko/controller/ProductController.java
@@ -0,0 +1,102 @@
+package com.telusko.controller;
+
+import com.telusko.model.Product;
+import com.telusko.service.ProductService;
+import com.telusko.utils.ImageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> productList = productService.getAllProducts();
+        return new ResponseEntity<>(productList, HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Integer id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @PostMapping("/product")
+    public ResponseEntity<?> createProduct(@RequestPart("product") Product product,
+                                           @RequestPart("imageFile") MultipartFile imageFile) {
+        try {
+            System.out.println(product.isProductAvailable());
+            Product savedProduct = productService.createProduct(product, imageFile);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable Integer productId) {
+        Product product = productService.getProductById(productId);
+        if (product != null && product.getImageData() != null) {
+            byte[] imageData = ImageUtils.decompressImage(product.getImageData());
+            return ResponseEntity.ok()
+                    .contentType(MediaType.valueOf(product.getImageType()))
+                    .body(imageData);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable Integer id,
+                                                @RequestPart("product") Product product,
+                                                @RequestPart("imageFile") MultipartFile imageFile) throws Exception {
+        Product updatedProduct = productService.updateProduct(id, product, imageFile);
+        if (updatedProduct != null) {
+            return new ResponseEntity<>("Product updated successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Failed to update product", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Integer id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            productService.deleteProduct(id);
+            return new ResponseEntity<>("Product deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+//    {
+//        "name": "Example Product",
+//            "description": "This is a detailed description of the example product.",
+//            "brand": "ExampleBrand",
+//            "price": 19.99,
+//            "category": "Electronics",
+//            "stockQuantity": 100,
+//            "releaseDate": "2024-04-17",
+//            "productAvailable": true
+//    }
+
+}
