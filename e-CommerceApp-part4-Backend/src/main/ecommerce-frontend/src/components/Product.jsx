8e4a976de3a6989ec4981de6e9622a4370ede036
GAURAV SHARMA <gsharma1154@gmail.com>
2024-05-25 17:27:50 +0530

    final project

--- e-CommerceApp-part4-Backend/src/main/ecommerce-frontend/src/components/Product.jsx
+++ e-CommerceApp-part4-Backend/src/main/ecommerce-frontend/src/components/Product.jsx
@@ -2,11 +2,12 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { useState } from "react";
 import AppContext from "../Context/Context";
-import axios from "../axois";
+import axios from "../axios";
 import UpdateProduct from "./UpdateProduct";
 const Product = () => {
   const { id } = useParams();
-  const { data } = useContext(AppContext);
+  const { data, addToCart, removeFromCart, cart, refreshData } =
+    useContext(AppContext);
   const [product, setProduct] = useState(null);
   const [imageUrl, setImageUrl] = useState("");
   const navigate = useNavigate();
@@ -20,7 +21,6 @@ const Product = () => {
         setProduct(response.data);
         if (response.data.imageName) {
           fetchImage();
-          console.log(response.data.imageName);
         }
       } catch (error) {
         console.error("Error fetching product:", error);
@@ -33,45 +33,32 @@ const Product = () => {
         { responseType: "blob" }
       );
       setImageUrl(URL.createObjectURL(response.data));
-      console.log(response.data);
     };
 
     fetchProduct();
   }, [id]);
 
-  console.log("URL Parameter ID:", id);
-  // console.log("Product Data:", data);
-
-  
-
-  const deleteProduct =  () => {
+  const deleteProduct = async () => {
     try {
-       axios.delete(`http://localhost:8080/api/product/${id}`);
-      navigate("/"); // Redirect to a different route after successful deletion
+      await axios.delete(`http://localhost:8080/api/product/${id}`);
+      removeFromCart(id);
       console.log("Product deleted successfully");
       alert("Product deleted successfully");
+      refreshData();
+      navigate("/");
     } catch (error) {
       console.error("Error deleting product:", error);
-      if (error.response) {
-        // Error if i am getting it from backend
-        console.error("Backend error:", error.response.data);
-        alert("Failed to delete product. Backend error.");
-      } else if (error.request) {
-        // error if network errors
-        console.error("Network error:", error.request);
-        alert("Failed to delete product. Network error.");
-      } else {
-        // error if Handle other errors
-        console.error("Other error:", error.message);
-        alert("Failed to delete product. Please try again.");
-      }
     }
   };
-  
+
   const handleEditClick = () => {
-    navigate(`/product/update/${id}`); 
+    navigate(`/product/update/${id}`);
   };
 
+  const handlAddToCart = () => {
+    addToCart(product);
+    alert("Product added to cart");
+  };
   if (!product) {
     return (
       <h2 className="text-center" style={{ padding: "10rem" }}>
@@ -82,14 +69,12 @@ const Product = () => {
   return (
     <>
       <div className="containers">
-        {/* <div className="left-column-img"> */}
         <img
           className="left-column-img"
           src={imageUrl}
           alt={product.imageName}
         />
 
-        {/* </div> */}
         <div className="right-column">
           <div className="product-description">
             <span>{product.category}</span>
@@ -100,9 +85,15 @@ const Product = () => {
 
           <div className="product-price">
             <span>{"$" + product.price}</span>
-            <a href="#" className="cart-btn">
-              Add to cart
-            </a>
+            <button
+              className={`cart-btn ${
+                !product.productAvailable ? "disabled-btn" : ""
+              }`}
+              onClick={handlAddToCart}
+              disabled={!product.productAvailable}
+            >
+              {product.productAvailable ? "Add to cart" : "Out of Stock"}
+            </button>
             <h6>
               Stock Available :{" "}
               <i style={{ color: "green", fontWeight: "bold" }}>
@@ -115,9 +106,13 @@ const Product = () => {
             </p>
           </div>
           <div className="update-button ">
-            <button className="btn btn-primary" type="button"  onClick={handleEditClick}>
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={handleEditClick}
+            >
               Update
-            </button> 
+            </button>
             {/* <UpdateProduct product={product} onUpdate={handleUpdate} /> */}
             <button
               className="btn btn-primary"
